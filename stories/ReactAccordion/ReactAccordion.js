06471b4a8b21a8c0b8aec73f80b7b57949e5892d
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-04-28 08:48:40 +1000

    Just commit

---
+++ stories/ReactAccordion/ReactAccordion.js
@@ -0,0 +1,101 @@
+
+import React, { useState } from "react";
+import "./styles.css";
+
+// Define ChevronUp SVG icon component
+const ChevronUp = () => (
+    <svg aria-hidden="true" viewBox="0 0 20 20" width="20" height="20">
+      <path d="M5 8l5 5 5-5H5z" fill="currentColor" />
+    </svg>
+  );
+
+// Define ChevronDown SVG icon component
+const ChevronDown = () => (
+    <svg aria-hidden="true" viewBox="0 0 20 20" width="20" height="20">
+      <path d="M5 12l5-5 5 5H5z" fill="currentColor" />
+    </svg>
+  );
+
+// Define ChevronDown SVG icon component
+const Accordion = ({ items, groupName, multiExpand }) => {
+    const [activeIndices, setActiveIndices] = useState([]);
+    
+    // Handle title click event
+    const onTitleClick = (index) => {
+      if (multiExpand) {
+        setActiveIndices((prevIndices) =>
+          prevIndices.includes(index)
+            ? prevIndices.filter((i) => i !== index)
+            : [...prevIndices, index]
+        );
+      } else {
+        setActiveIndices(index === activeIndices[0] ? [] : [index]);
+      }
+    };
+  
+    // Handle title keydown event
+    const onTitleKeyDown = (event, index) => {
+      const key = event.key;
+  
+      if (key === "Enter" || key === " ") {
+        onTitleClick(index);
+        event.preventDefault();
+      } else if (key === "ArrowDown" || key === "ArrowUp") {
+        const newIndex =
+          key === "ArrowDown"
+            ? (index + 1) % items.length
+            : (index - 1 + items.length) % items.length;
+        const newButton = document.getElementById(
+          `${groupName}-heading-${newIndex}`
+        );
+        newButton.focus();
+        event.preventDefault();
+      } else if (key === "Home") {
+        const firstButton = document.getElementById(`${groupName}-heading-0`);
+        firstButton.focus();
+        event.preventDefault();
+      } else if (key === "End") {
+        const lastButton = document.getElementById(
+          `${groupName}-heading-${items.length - 1}`
+        );
+        lastButton.focus();
+        event.preventDefault();
+      }
+    };
+  
+    // Render accordion items
+    const renderedItems = items.map((item, index) => {
+      const active = activeIndices.includes(index) ? "active" : "";
+  
+      // Return accordion markup
+      return (
+        <React.Fragment key={item.title}>
+          <h3>
+            <button
+              className={`title ${active}`}
+              onClick={() => onTitleClick(index)}
+              onKeyDown={(event) => onTitleKeyDown(event, index)}
+              id={`${groupName}-heading-${index}`}
+              aria-controls={`${groupName}-panel-${index}`}
+              aria-expanded={active === "active"}
+            >
+              <span>{item.title}</span>
+              {active ? <ChevronDown /> : <ChevronUp />}
+            </button>
+          </h3>
+          <div
+            className={`content ${active}`}
+            id={`${groupName}-panel-${index}`}
+            aria-labelledby={`${groupName}-heading-${index}`}
+            hidden={active !== "active"}
+          >
+            <p>{item.content}</p>
+          </div>
+        </React.Fragment>
+      );
+    });
+  
+    return <div className="ui styled accordion">{renderedItems}</div>;
+  };
+
+export default Accordion;
---
+++ stories/ReactAccordion/ReactAccordion.stories.js
@@ -0,0 +1,102 @@
+import React from "react";
+import Accordion from "./ReactAccordion";
+
+export default {
+  title: "Components/ReactAccordion",
+  component: Accordion,
+  argTypes: {
+    multiExpand: {
+      control: "boolean",
+      description: "Allow multiple accordions to be open at once",
+      defaultValue: false,
+    },
+  },
+};
+
+const Template = (args) => (
+    <div role="region" aria-labelledby={`${args.groupName}-heading`}>
+      <h2 id={`${args.groupName}-heading`}>{args.groupName.charAt(0).toUpperCase() + args.groupName.slice(1)}</h2>
+      <Accordion {...args} />
+    </div>
+  );
+
+export const Cats = Template.bind({});
+export const Dogs = Template.bind({});
+export const Horses = Template.bind({});
+
+Cats.args = {
+  items: [
+    {
+      title: "Persian",
+      content:
+        "Persian cats are known for their long, luxurious fur and flat faces.",
+    },
+    {
+      title: "Siamese",
+      content:
+        "Siamese cats have sleek, short coats and striking blue almond-shaped eyes.",
+    },
+    {
+      title: "Maine Coon",
+      content:
+        "Maine Coon cats are large, friendly cats with tufted ears and bushy tails.",
+    },
+    {
+      title: "Bengal",
+      content:
+        "Bengal cats are known for their wild appearance and energetic nature.",
+    },
+  ],
+  groupName: "cats",
+  multiExpand: false,
+};
+
+Dogs.args = {
+  items: [
+    {
+      title: "Golden Retriever",
+      content: "Golden Retrievers are friendly, intelligent, and devoted.",
+    },
+    {
+      title: "Labrador Retriever",
+      content: "Labrador Retrievers are friendly, active, and outgoing.",
+    },
+    {
+      title: "German Shepherd",
+      content: "German Shepherds are confident, courageous, and smart.",
+    },
+    {
+      title: "Bulldog",
+      content: "Bulldogs are friendly, courageous, and calm.",
+    },
+  ],
+  groupName: "dogs",
+  multiExpand: false,
+};
+
+Horses.args = {
+  items: [
+    {
+      title: "Arabian",
+      content:
+        "Arabian horses are known for their elegance, intelligence, and spirited nature.",
+    },
+    {
+      title: "Thoroughbred",
+      content:
+        "Thoroughbreds are athletic, fast, and primarily used for racing and jumping competitions.",
+    },
+    {
+      title: "Appaloosa",
+      content:
+        "Appaloosas are recognized by their unique spotted coat patterns and are versatile in various disciplines.",
+    },
+    {
+      title: "Clydesdale",
+      content:
+        "Clydesdales are large, strong horses known for their feathered legs and gentle temperament.",
+    },
+  ],
+  groupName: "horses",
+  multiExpand: true,
+};
