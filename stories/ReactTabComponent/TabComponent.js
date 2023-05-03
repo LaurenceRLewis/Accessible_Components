fe731abf069135cd9e320e25daaed932960482ce
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-05-03 14:20:04 +1000

    Add Accordion Tab control

--- stories/ReactTabComponent/TabComponent.js
+++ stories/ReactTabComponent/TabComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./TabComponent.module.css";
 
 const Tab = ({ id, title, isSelected, onClick, onKeyDown, textColor, tabBackground }) => (
@@ -26,6 +26,7 @@ const TabPanel = ({ id, isSelected, children, tabIndex }) => (
     aria-labelledby={`${id}`}
     hidden={!isSelected}
     tabIndex={tabIndex}
+    data-expanded={isSelected}
   >
     {children}
   </div>
@@ -42,48 +43,64 @@ export const TabComponent = ({
   },
 }) => {
   const [selectedTab, setSelectedTab] = useState(tabs[0].id);
+  const [isAccordionView, setIsAccordionView] = useState(window.innerWidth < 400);
+
+  const handleWindowResize = () => {
+    setIsAccordionView(window.innerWidth < 400);
+  };
+
+  const windowResizeEvent = useRef(null);
+
+  useEffect(() => {
+    windowResizeEvent.current = handleWindowResize;
+    window.addEventListener("resize", windowResizeEvent.current);
+
+    return () => {
+      window.removeEventListener("resize", windowResizeEvent.current);
+    };
+  }, []);
 
   const handleKeyDown = (id, event) => {
     const currentIndex = tabs.findIndex((tab) => tab.id === id);
-    
-        if (event.key === "ArrowRight") {
-          event.preventDefault();
-          const nextIndex = (currentIndex + 1) % tabs.length;
-          if (triggerActivation === "automated") {
-            setSelectedTab(tabs[nextIndex].id);
-          }
-          document.getElementById(tabs[nextIndex].id).focus();
-        } else if (event.key === "ArrowLeft") {
-          event.preventDefault();
-          const prevIndex = (currentIndex - 1 + tabs.length) % tabs.length;
-          if (triggerActivation === "automated") {
-            setSelectedTab(tabs[prevIndex].id);
-          }
-          document.getElementById(tabs[prevIndex].id).focus();
-        } else if (event.key === "Home") {
-          event.preventDefault();
-          if (triggerActivation === "automated") {
-            setSelectedTab(tabs[0].id);
-          }
-          document.getElementById(tabs[0].id).focus();
-        } else if (event.key === "End") {
-          event.preventDefault();
-          if (triggerActivation === "automated") {
-            setSelectedTab(tabs[tabs.length - 1].id);
-          }
-          document.getElementById(tabs[tabs.length - 1].id).focus();
-        } else if (
-          (event.key === "Enter" || event.key === " ") &&
-          triggerActivation === "manual"
-        ) {
-          event.preventDefault();
-          setSelectedTab(id);
-        }
-      };
+
+    if (event.key === "ArrowRight") {
+      event.preventDefault();
+      const nextIndex = (currentIndex + 1) % tabs.length;
+      if (triggerActivation === "automated") {
+        setSelectedTab(tabs[nextIndex].id);
+      }
+      document.getElementById(tabs[nextIndex].id).focus();
+    } else if (event.key === "ArrowLeft") {
+      event.preventDefault();
+      const prevIndex = (currentIndex - 1 + tabs.length) % tabs.length;
+      if (triggerActivation === "automated") {
+        setSelectedTab(tabs[prevIndex].id);
+      }
+      document.getElementById(tabs[prevIndex].id).focus();
+    } else if (event.key === "Home") {
+      event.preventDefault();
+      if (triggerActivation === "automated") {
+        setSelectedTab(tabs[0].id);
+      }
+      document.getElementById(tabs[0].id).focus();
+    } else if (event.key === "End") {
+      event.preventDefault();
+      if (triggerActivation === "automated") {
+        setSelectedTab(tabs[tabs.length - 1].id);
+      }
+      document.getElementById(tabs[tabs.length - 1].id).focus();
+    } else if (
+      (event.key === "Enter" || event.key === " ") &&
+      triggerActivation === "manual"
+    ) {
+      event.preventDefault();
+      setSelectedTab(id);
+    }
+  };
 
   return (
     <div className={styles.tabContainer}>
-    <div role="tablist" aria-label="HTML and ARIA information" className={styles.tabList}>
+      <div role="tablist" aria-label="HTML and ARIA information" className={styles.tabList}>
         {tabs.map(({ id, title }) => (
           <Tab
             key={id}
