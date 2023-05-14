212f1f7d0bc51d29b6e1165ddaaa5863e69f360d
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-05-14 10:21:39 +1000

    Update to  Storybook 7.0.11

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,19 @@
-const config = {
-  stories: ["../stories/**/*.mdx", "../stories/**/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-controls",
-    "@storybook/addon-mdx-gfm",
-    "@storybook/addon-a11y",
-  ],
+const path = require("path");
+module.exports = {
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-controls", "@storybook/addon-docs", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  webpackFinal: async config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src")
+    };
+    return config;
+  },
   framework: {
     name: "@storybook/react-webpack5",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: true
+  }
 };
-
-export default config;
