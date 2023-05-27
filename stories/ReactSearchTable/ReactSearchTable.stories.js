7f695635fcef27bf947afcd509ecc883feb73a78
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-05-28 08:42:08 +1000

    Rename search table menu item

--- stories/ReactSearchTable/ReactSearchTable.stories.js
+++ stories/ReactSearchTable/ReactSearchTable.stories.js
@@ -3,7 +3,7 @@ import ReactSearchTable from './ReactSearchTable';
 import reactSearchTableDescription from './ReactSearchTableDescription';
 
 export default {
-  title: 'Tables/React Table with search function',
+  title: 'Tables/React Table with Search',
   component: ReactSearchTable,
   parameters: {
     docs: {
