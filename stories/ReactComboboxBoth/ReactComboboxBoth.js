de9b64f0d93d26eb048a50cabd01b48b52a6ecc1
Laurence Lewis <laurence.r.lewis@gmail.com>
2023-06-04 07:11:03 +1000

    Predictive text bug fixes

--- stories/ReactComboboxBoth/ReactComboboxBoth.js
+++ stories/ReactComboboxBoth/ReactComboboxBoth.js
@@ -1,32 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react';
-import styles from "./ReactComboboxBoth.module.css";
+import styles from './ReactComboboxBoth.module.css';
 
 const townsAndCities = [
-  "Adelaide (SA)",
-  "Albany (WA)",
-  "Ayr (QLD)",
-  "Brisbane (QLD)",
-  "Bundaberg (QLD)",
-  "Cairns (QLD)",
-  "Charters Towers (QLD)",
-  "Dalby (QLD)",
-  "Gladstone (QLD)",
-  "Gold Coast (QLD)",
-  "Gympie (QLD)",
-  "Hervey Bay (QLD)",
-  "Innisfail (QLD)",
-  "Mackay (QLD)",
-  "Maryborough (QLD)",
-  "Melbourne (VIC)",
-  "Moranbah (QLD)",
-  "Mount Isa (QLD)",
-  "Perth (WA)",
-  "Rockhampton (QLD)",
-  "Sunshine Coast (QLD)",
-  "Sydney (NSW)",
-  "Toowoomba (QLD)",
-  "Townsville (QLD)",
-  "Warwick (QLD)",
+  'Adelaide (SA)',
+  'Albany (WA)',
+  'Ayr (QLD)',
+  'Brisbane (QLD)',
+  'Bundaberg (QLD)',
+  'Cairns (QLD)',
+  'Charters Towers (QLD)',
+  'Dalby (QLD)',
+  'Gladstone (QLD)',
+  'Gold Coast (QLD)',
+  'Gympie (QLD)',
+  'Hervey Bay (QLD)',
+  'Innisfail (QLD)',
+  'Mackay (QLD)',
+  'Maryborough (QLD)',
+  'Melbourne (VIC)',
+  'Moranbah (QLD)',
+  'Mount Isa (QLD)',
+  'Perth (WA)',
+  'Rockhampton (QLD)',
+  'Sunshine Coast (QLD)',
+  'Sydney (NSW)',
+  'Toowoomba (QLD)',
+  'Townsville (QLD)',
+  'Warwick (QLD)',
 ];
 
 const ReactComboboxBoth = () => {
@@ -35,11 +35,12 @@ const ReactComboboxBoth = () => {
   const [userInput, setUserInput] = useState('');
   const [listboxVisible, setListboxVisible] = useState(false);
   const [predictiveText, setPredictiveText] = useState('');
+  const [textboxEmpty, setTextboxEmpty] = useState(true); // New state variable
 
   const inputRef = useRef(null);
 
   const filterOptions = (query) => {
-    return townsAndCities.filter(option => option.toLowerCase().startsWith(query.toLowerCase()));
+    return townsAndCities.filter((option) => option.toLowerCase().startsWith(query.toLowerCase()));
   };
 
   const showOptions = () => {
@@ -65,14 +66,15 @@ const ReactComboboxBoth = () => {
   const onInput = (event) => {
     const newValue = event.target.value;
     setUserInput(newValue);
-  
+
     const newFilteredOptions = filterOptions(newValue);
     setFilteredOptions(newFilteredOptions);
-  
+
     if (newValue === '') {
-      hideOptions();
+      setTextboxEmpty(true); // Update textboxEmpty state when the value is empty
       setPredictiveText('');
     } else {
+      setTextboxEmpty(false); // Update textboxEmpty state when the value is not empty
       showOptions();
       if (newFilteredOptions.length > 0) {
         setSelectedIndex(0);
@@ -82,7 +84,7 @@ const ReactComboboxBoth = () => {
       }
     }
   };
-  
+
   const onKeydown = (event) => {
     switch (event.key) {
       case 'ArrowDown':
@@ -112,30 +114,23 @@ const ReactComboboxBoth = () => {
       case 'Backspace':
         if (userInput !== '') {
           event.preventDefault();
-          const newValue = userInput.slice(0, -1);
-          setUserInput(newValue);
-          setPredictiveText(newValue);
-          const newFilteredOptions = filterOptions(newValue);
-          setFilteredOptions(newFilteredOptions);
-          if (newFilteredOptions.length > 0) {
-            setSelectedIndex(0);
-          } else {
+          if (userInput.length === 1) {
+            setUserInput('');
+            setPredictiveText('');
+            setFilteredOptions([...townsAndCities]);
             setSelectedIndex(-1);
-          }
-        }
-        break;
-      case 'Delete':
-        if (userInput !== '') {
-          event.preventDefault();
-          const newValue = userInput.slice(1);
-          setUserInput(newValue);
-          setPredictiveText(newValue);
-          const newFilteredOptions = filterOptions(newValue);
-          setFilteredOptions(newFilteredOptions);
-          if (newFilteredOptions.length > 0) {
-            setSelectedIndex(0);
           } else {
-            setSelectedIndex(-1);
+            const newValue = userInput.slice(0, -1);
+            setUserInput(newValue);
+            const newFilteredOptions = filterOptions(newValue);
+            setFilteredOptions(newFilteredOptions);
+            if (newFilteredOptions.length > 0) {
+              setSelectedIndex(0);
+              setPredictiveText(newFilteredOptions[0]);
+            } else {
+              setSelectedIndex(-1);
+              setPredictiveText('');
+            }
           }
         }
         break;
@@ -160,42 +155,61 @@ const ReactComboboxBoth = () => {
     }
   }, [selectedIndex, userInput, filteredOptions]);
 
+  useEffect(() => {
+    if (textboxEmpty) {
+      setSelectedIndex(-1); // Reset selectedIndex to -1 when the textbox is empty
+      hideOptions(); // Close the listbox when the textbox is empty
+    }
+  }, [textboxEmpty]);
+
   return (
-    <div className={styles["comboboxContainer"]}>
-      <label id={styles["combobox-label"]} htmlFor={styles["combobox-input"]}>
+    <div className={styles['comboboxContainer']}>
+      <label id={styles['combobox-label']} htmlFor={styles['combobox-input']}>
         Australian cities and towns
       </label>
-      <div className={styles["optionsCount"]} aria-live="assertive" aria-atomic="true" role="region">
+      <div className={styles['optionsCount']} aria-live="assertive" aria-atomic="true" role="region">
         {listboxVisible ? `${filteredOptions.length} options available.` : ''}
       </div>
-      <div className={styles["comboboxWrapper"]}>
-      <input
-        id={styles["combobox-input"]}
-        type="text"
-        autocomplete="off"
-        role="combobox"
-        aria-autocomplete="both"
-        onKeyDown={onKeydown}
-        onInput={onInput}
-        value={predictiveText}
-        ref={inputRef}
-      />
-        <button id={styles["toggle-button"]} aria-label="Show options" tabIndex="-1" onClick={onToggleButtonClick}>
+      <div className={styles['comboboxWrapper']}>
+        <input
+          id={styles['combobox-input']}
+          type="text"
+          autoComplete="off"
+          role="combobox"
+          aria-autocomplete="both"
+          onKeyDown={onKeydown}
+          onInput={onInput}
+          value={predictiveText}
+          ref={inputRef}
+        />
+        <button
+          id={styles['toggle-button']}
+          aria-label="Show options"
+          tabIndex="-1"
+          onClick={onToggleButtonClick}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48">
             <path d="M7 10l5 5 5-5z" fill="#ffffff" />
           </svg>
         </button>
       </div>
       <ul
-        id={styles["combobox-listbox"]}
+        id={styles['combobox-listbox']}
         role="listbox"
         aria-label="Australian towns and cities"
-        style={{display: listboxVisible ? 'block' : 'none'}}
+        style={{ display: listboxVisible ? 'block' : 'none' }}
       >
-        {filteredOptions.map((option, index) => 
-          <li role="option" aria-selected={selectedIndex === index ? "true" : "false"} onClick={() => selectOption(index)} key={index} className={selectedIndex === index ? styles["selectedOption"] : ''}>
+        {filteredOptions.map((option, index) => (
+          <li
+            role="option"
+            aria-selected={selectedIndex === index ? 'true' : 'false'}
+            onClick={() => selectOption(index)}
+            key={index}
+            className={selectedIndex === index ? styles['selectedOption'] : ''}
+          >
             {option}
-          </li>)}
+          </li>
+        ))}
       </ul>
     </div>
   );
