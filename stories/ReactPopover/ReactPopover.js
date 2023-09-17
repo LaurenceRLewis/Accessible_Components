ecdda4af73444ec0004338e042524fe5482967c0
LaurenceRLewis <laurence.r.lewis@gmail.com>
2023-09-17 10:41:44 +1000

    Push

--- stories/ReactPopover/ReactPopover.js
+++ stories/ReactPopover/ReactPopover.js
@@ -38,15 +38,6 @@ const Popover = ({
     }, 0);
   };
 
-  const handleEscKey = (event) => {
-    if (event.key === "Escape" && (popoverRef.current.contains(event.target) || event.target === buttonRef.current)) {
-      setIsOpen(false);
-      buttonRef.current.focus();
-    }
-  };
-    }
-  };
-
   const handleArrowKeyNavigation = (event) => {
     if (
       useArrowKeys &&
